ec586a2efe0e6fedeb97a9e67467613cb637965c
Timur Darii <timurdarii95@gmail.com>
2026-02-21 17:35:13 +0000

    feat:Populate DB.py

--- db.py
+++ db.py
@@ -8,3 +8,22 @@ connection = mysql.connector.connect(
 )
 
 cursor = connection.cursor()
+
+cursor.execute("""
+CREATE TABLE students(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(25) NOT NULL
+)
+""")
+
+cursor.execute("""
+CREATE TABLE tasks(
+     in INT AUTO_INCREMENT PRIMARY KEY,
+     Student_id INT,
+     title VARCHAR(255),
+     status VARCHAR(20),
+     FOREIGN KEY (student_in) REFERENCES students(id)
+)
+""")
+
+connection.commit()
